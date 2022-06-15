610156a51ebade683735ad12742f0819ddee94bd
danielbrito <daniell-henrique@hotmail.com>
2022-06-14 23:09:24 -0300

    refactor: created Money and Portfolio modules; improved tests

---
+++ portfolio.js
@@ -0,0 +1,21 @@
+const Money = require("./money");
+
+class Portfolio {
+  constructor() {
+    this.moneys = [];
+  }
+
+  add(...moneys) {
+    return (this.moneys = this.moneys.concat(moneys));
+  }
+
+  evaluate(currency) {
+    let total = this.moneys.reduce((sum, money) => {
+      return sum + money.amount;
+    }, 0);
+
+    return new Money(total, currency);
+  }
+}
+
+module.exports = Portfolio;
