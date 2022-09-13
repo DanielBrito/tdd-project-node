02633c9df69205259c42c161366c69d8fc50c3ea
danielbrito <daniell-henrique@hotmail.com>
2022-09-13 20:36:24 -0300

    feat: Improved error handling for missing exchange rates

--- portfolio.js
+++ portfolio.js
@@ -10,11 +10,25 @@ class Portfolio {
   }
 
   evaluate(currency) {
+    let failures = [];
+
     let total = this.moneys.reduce((sum, money) => {
-      return sum + this.convert(money, currency);
+      let convertedAmount = this.convert(money, currency);
+
+      if (convertedAmount === undefined) {
+        failures.push(`${money.currency}->${currency}`);
+
+        return sum;
+      }
+
+      return sum + convertedAmount;
     }, 0);
 
-    return new Money(total, currency);
+    if (!failures.length) {
+      return new Money(total, currency);
+    }
+
+    throw new Error(`Missing exchange rate(s): [${failures.join()}]`);
   }
 
   convert(money, currency) {
@@ -28,8 +42,13 @@ class Portfolio {
     }
 
     let key = `${money.currency}->${currency}`;
+    let rate = exchangeRates.get(key);
 
-    return money.amount * exchangeRates.get(key);
+    if (rate === undefined) {
+      return undefined;
+    }
+
+    return money.amount * rate;
   }
 }
 
